042d68dabe4b1914b27678879f11cd75f99a4402
PriMoro <prisu03@gmail.com>
2022-04-01 01:40:56 -0300

    back-end

--- api/src/routes/pokemons.js
+++ api/src/routes/pokemons.js
@@ -1,5 +1,6 @@
 const { Router } = require("express");
 const getAll = require("../controllers/pokemons/all");
+const { Pokemon, Type } = require("../db");
 const router = Router();
 
 router.get("/", async (req, res) => {
@@ -30,6 +31,48 @@ router.get("/:id", async (req, res) => {
     }
   } catch (error) {
     res.status(400).send(error);
+  }
+});
+router.post("/", async (req, res) => {
+  let {
+    name,
+    img,
+    hp,
+    attack,
+    defense,
+    speed,
+    height,
+    weight,
+    ifWasCreated,
+    type,
+  } = req.body;
+  try {
+    if (name && typeof name === "string") {
+      let pokeCreated = await Pokemon.create({
+        name,
+        img,
+        hp,
+        attack,
+        defense,
+        speed,
+        height,
+        weight,
+        ifWasCreated,
+      });
+      let pokeType = await Type.findAll({ where: { name: type } });
+      //pokeType = pokeType.map((e) => e.name);
+      // let pt = pokeType.map((el) => {
+      //   for (let i = 0; i < el.length; i++) {
+      //     return el[i.name];
+      //   }
+      // });
+      pokeCreated.addType(pokeType);
+      res.send("poke created successfully");
+    } else {
+      res.status(404).send("all fields are required");
+    }
+  } catch (err) {
+    res.status(404).send(err);
   }
 });
 module.exports = router;
