979e5a6fe14b2b67408b2ff4947c2d22f7f33e53
PriMoro <prisu03@gmail.com>
2022-04-15 17:54:47 -0300

    back-end

--- api/src/controllers/pokemons/allApi.js
+++ api/src/controllers/pokemons/allApi.js
@@ -4,7 +4,13 @@ const axios = require("axios");
 const getAllApi = async () => {
   const first20 = await axios.get("https://pokeapi.co/api/v2/pokemon");
   const second20 = await axios.get(first20.data.next);
+  // const third20 = await axios.get(second20.data.next);
+  // const fourth20 = await axios.get(third20.data.next);
+  // const fifth20 = await axios.get(fourth20.data.next);
   const all40 = first20.data.results.concat(second20.data.results);
+  // const all40_2 = third20.data.results.concat(fourth20.data.results);
+  // const all80 = all40_1.concat(all40_2);
+  // const all100 = all80.concat(fifth20.data.results);
 
   for (let p of all40) {
     let url = await axios.get(p.url);
@@ -12,8 +18,8 @@ const getAllApi = async () => {
     p.id = url.data.id;
     p.img = url.data.sprites.other.dream_world.front_default;
     //p.img =
-    url.data.sprites.versions["generation-v"]["black-white"].animated
-      .front_shiny;
+    //url.data.sprites.versions["generation-v"]["black-white"].animated
+    // .front_shiny;
     //p.img = url.data.sprites.front_default;
     p.hp = url.data.stats[0].base_stat;
     p.attack = url.data.stats[1].base_stat;
